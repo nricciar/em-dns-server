aecfcc74f29d263ac05ab64ded42aa790914c4f7
David Ricciardi <nricciar@facebook.(none)>
2010-12-04 03:21:40 +0000

    added some comments

--- server.rb
+++ server.rb
@@ -12,6 +12,10 @@ module DNSServer
 
   def self.init()
     @@GEOIP = GeoIP.new('GeoLiteCity.dat')
+
+    # the following is NOT an example of how to parse a bind zone file
+    # some things will be completly ignored and anything exotic will 
+    # likely cause the following code to fail.
     Dir.entries("zones").each do |file|
       if file =~ /^(.*).zone$/
         zonefile = File.read("zones/#{file}")
@@ -31,6 +35,7 @@ module DNSServer
     msg.question.each do |question|
       query = question.qname.to_s
 
+      # load the zone information for the current question
       @@ZONEMAP.each { |key,value| domain = key if query =~ /#{key}$/ }
       zone_records = @@ZONEMAP[domain] unless domain.nil?
 
@@ -38,25 +43,32 @@ module DNSServer
         puts "Q: #{query}"
         query.gsub!(/#{domain}/,"")
         query = query == "" ? "@" : query.chomp(".")
+
         match_distance = nil
         match_record = nil
         match_address = nil
 
         zone_records.each do |rr|
           if rr[1] == query.to_s && rr[4] == question.qclass.to_s && rr[5] == question.qtype.to_s
+            # get the location information for the current record
             rr_geo = @@GEOIP.country(rr.last)
             distance = rr_geo.nil? ? 0 : haversine_distance(geoip_data[9],geoip_data[10],rr_geo[9],rr_geo[10])["mi"].to_i
+
+            # if this is the first match or if we have found a match closer
+            # to the client
             if match_record.nil? || match_distance.nil? || match_distance > distance
               match_distance = distance
               match_record = rr[0]
               match_address = rr.last
             end
           elsif rr[1] == query && rr[5] == "CNAME" && question.qtype == "A"
+            # add the CNAME to our response, and then attempt to resolve the record
             msg.add_answer(Dnsruby::RR.create(rr[0].gsub(/@/,domain)))
             raise DnsRedirect, rr.last
           end
         end
         unless match_record.nil?
+          # the final result for the current question
           msg.add_answer(Dnsruby::RR.create(match_record.gsub(/@/,domain)))
           puts "#{question.qclass} #{question.qtype} #{question.qname.to_s} Resolved to #{match_address} -- Distance: #{match_distance}"
         end
@@ -67,12 +79,14 @@ module DNSServer
       end
     end
 
+    # mark the message as a response and send it to the client
     msg.header.qr = true
     msg.header.rcode = Dnsruby::RCode.NoError
     send_data msg.encode
   end
 
   protected
+  # mostly taken from http://www.codecodex.com/wiki/Calculate_distance_between_two_points_on_a_globe#Ruby
   def haversine_distance( lat1, lon1, lat2, lon2 )
     distances = Hash.new
     dlon = lon2 - lon1
