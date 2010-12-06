5eba2cd89e65c374e6081ee83d3148110aa2f392
David Ricciardi <nricciar@facebook.(none)>
2010-12-06 22:14:18 +0000

    better zone file parsing

--- lib/em-dns-server.rb
+++ lib/em-dns-server.rb
@@ -31,9 +31,6 @@ module DNSServer
   def self.init()
     @@GEOIP = GeoIP.new(geoip_data_path) unless !File.exists?(geoip_data_path)
 
-    # the following is NOT an example of how to parse a bind zone file
-    # some things will be completly ignored and anything exotic will 
-    # likely cause the following code to fail.
     Dir.entries("zones").each do |file|
       if file =~ /^(.*).zone$/
         zonefile = File.read("zones/#{file}")
@@ -42,32 +39,54 @@ module DNSServer
     end
   end
 
-  def self.reload_zone_file(filename)
-    @@ZONEMAP.merge!(self.parse_zone_file(filename))
-  end
-
   def self.parse_zone_file(filename)
     ret = {}
     origin = nil
     ttl = nil
     records = []
+    file = File.read(filename)
+    file.gsub!(/;.*$/,"") # strip comments
+    file.gsub!(/\t+/," ") # fold whitespace
 
-    file = File.new(filename,'r')
-    while (line = file.gets)
-      case line.split(";").first
+    file.split("\n").each do |line|
+      record = nil
+      case line
+      # $ORIGIN
       when /\$ORIGIN\s+([^\s]+)$/
         origin = $1
-      when /\$TTL\s+([\w]+)(.+)$/
-        ttl = $1
-      when /^(([\w@\*\-\.]+)\s+(([0-9]+)\s+|)([A-Za-z]+)\s+([A-Za-z]+)\s+(([0-9]+)\s+|)([\w@\-\.:]+))/
-        records << { :name => $2, :ttl => $4, :class => $5, :type => $6, 
-	  :priority => $8, :address => $9}
+      # $TTL
+      when /\$TTL\s+([^\s;]+)(;?[\d\s\w]+)?\s*/
+        ttl = get_ttl_from_string($1)
+      # MX
+      when /\A(|\*\.[\w\d\.]+|\*|\s*\@|\.|([-\w\d]+(\.[-\w\d]+)*\.?))
+	\s+ ((\d+|IN|HESIOD|CHAOS)\s+)? ((\d+|IN|HESIOD|CHAOS)\s+)?
+	(MX) \s+ (\d+) \s+ ([-\w\d]+((\.[-\w\d]+)*)?\.?) \s*$/mxi
+        record = { :name => $1, :type => $8, :priority => $9, :address => $10 }
+        record.merge!(fix_ttl_class($5,$7,ttl))
+      # PTR, NS, CNAME
+      when /\A(|\*|\w+|\s*\@|\.|([-\w\d]+(\.[-\w\d]+)*\.?))
+	\s+ ((\d+|IN|HESIOD|CHAOS)\s+)? ((\d+|IN|HESIOD|CHAOS)\s+)?
+	(PTR|NS|CNAME) \s+ ([-\w\d]+((\.[-\w\d]+)*)?\.?|\@) \s*$/mxi
+        record = { :name => $1, :type => $8, :address => $9 }
+        record.merge!(fix_ttl_class($5,$7,ttl))
+      # TXT, HINFO, AAAA
+      when /\A(|\*|\s*\@|\.|([-\w\d]+(\.[-\w\d]+)*\.?)?)
+	\s+ ((\d+|IN|HESIOD|CHAOS)\s+)? ((\d+|IN|HESIOD|CHAOS)\s+)?
+	(TXT|HINFO|AAAA) \s+ (".+?"|[:\d\w]+) \s*$/
+        record = { :name => $1, :type => $8, :address => $9 }
+        record.merge!(fix_ttl_class($5,$7,ttl))
+      # A
+      when /\A(|\*|\*.[-\w\d\.]+|[-\w\d\.]+|\s*\@|\.|[-\w\d]+(((\.[-\w\d]+)*)\.?)?)
+	\s+ ((\d+|IN|HESIOD|CHAOS)\s+)? ((\d+|IN|HESIOD|CHAOS)\s+)?
+	(A) \s+ (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) \s*$/mxi
+        record = { :name => $1, :type => $9, :address => $10 }
+        record.merge!(fix_ttl_class($6,$8,ttl))
       end
+      records << record unless record.nil?
     end
-    file.close
     origin = File.basename(filename, ".zone") if origin.nil?
     origin += "." if origin[-1,1] != "."
-    ret[origin] = { :records => records, :ttl => ttl }
+    ret[origin] = { :records => records, :ttl => ttl, :filename => filename, :mtime => File.mtime(filename) }
     ret
   end
 
@@ -94,6 +113,33 @@ module DNSServer
   end
 
   protected
+  def self.fix_ttl_class(ct1,ct2,ttl)
+    if ct1 =~ /^\d+$/
+      { :ttl => ct1, :class => (ct2 || 'IN') }
+    else
+      { :ttl => (ct2 || (ttl || 0)), :class => (ct1 || 'IN') }
+    end
+  end
+
+  def self.get_ttl_from_string(ttl)
+    case ttl.to_s
+    when /^([0-9]+)$/
+      ttl.to_i
+    when /^([0-9]+)D$/i
+      $1.to_i * 86400
+    when /^([0-9]+)H$/i
+      $1.to_i * 3600
+    when /^([0-9]+)W$/i
+      $1.to_i * 604800
+    when /^([0-9]+)S$/i
+      $1.to_i
+    when /^([0-9]+)M$/i
+      $1.to_i * 60
+    else
+      0
+    end
+  end
+
   def resolv(question,msg,geoip_data=nil)
     success = false
     query = question.qname.to_s
